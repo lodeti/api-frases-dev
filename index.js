c75b6837894e4a12b334e2c3e0db9d7a5aa96a76
André Lodeti <107418450+lodeti@users.noreply.github.com>
2023-08-28 10:39:35 -0300

    Adicionando paginação

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -9,29 +9,46 @@ let frases = [];
 
 async function lerFrases() {
   try {
-    const data = await fs.readFile(path.join(__dirname, 'frases.json'), 'utf8');
+    const data = await fs.readFile(path.join(__dirname, "frases.json"), "utf8");
     frases = JSON.parse(data).frases;
   } catch (err) {
-    console.error('Erro ao ler o arquivo frases.json', err);
-    throw new Error('Erro ao ler as frases.');
+    console.error("Erro ao ler o arquivo frases.json", err);
+    throw new Error("Erro ao ler as frases.");
   }
 }
 
 app.use(cors());
-
 
 function selecionarFraseAleatoria() {
   const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
   return fraseAleatoria;
 }
 
-app.get('/frase', (req, res) => {
+app.get("/", (req, res) => {
+  res.send(
+    "Bem-vindo à API Frases Dev! Esta API é bem simples e foi construída, a princípio, apenas para uso em projetos pessoais simples. O endpoint `/dica` exibirá uma frase com uma dica tech!"
+  );
+});
+
+app.get("/dica", (req, res) => {
   const fraseAleatoria = selecionarFraseAleatoria();
   res.send(fraseAleatoria);
 });
 
-app.get('/', (req, res) => {
-  res.send('Bem-vindo à API Frases Dev! Esta API é bem simples e foi construída, a princípio, apenas para uso em projetos pessoais simples. O endpoint /frase exibirá uma frase com uma dica tech!');
+app.get("/frases", (req, res) => {
+  const pagina = parseInt(req.query.pagina) || 1;
+  const quantidade = parseInt(req.query.quantidade) || 10;
+
+  const startIndex = (pagina - 1) * quantidade;
+  const endIndex = startIndex + quantidade;
+
+  const frasesPaginadas = frases.slice(startIndex, endIndex);
+
+  res.send({
+    currentPage: pagina,
+    quantidade: quantidade,
+    frases: frasesPaginadas,
+  });
 });
 
 async function startServer() {
